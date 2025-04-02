5035c641ca8ddd011a9b50b7f4886afe1fa15815
IkuroMaira <gwen.bussac@gmail.com>
2025-04-02 04:46:53 +0200

    Display menu on the front

--- module_1_katas/session-kata-adalicious-21-3-25-IkuroMaira/front-client/src/components/customersView/MenuPage.jsx
+++ module_1_katas/session-kata-adalicious-21-3-25-IkuroMaira/front-client/src/components/customersView/MenuPage.jsx
@@ -1,14 +1,29 @@
 import CardPlate from "../menu/CardPlate.jsx";
 import Header from "../common/Header.jsx";
 import styles from "./MenuPage.module.css"
+import React, { useState, useEffect } from "react";
 
 export default function MenuPage() {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        const fetchMenu = async () => {
+            const response = await fetch('http://localhost:5002/menu');
+            const data = await response.json();
+            setMenu(data);
+        };
+
+        fetchMenu();
+    }, []);
+
     return (
         <>
             <Header />
 
             <h1 className={styles.firstNameTitle}>Bonjour {/*{firstName}*/}</h1>
-            <CardPlate />
+            {menu.map((plate) => (
+                <CardPlate key={plate.id} plate={plate} />
+            ))}
         </>
     )
 }
--- module_1_katas/session-kata-adalicious-21-3-25-IkuroMaira/front-client/src/components/menu/CardPlate.jsx
+++ module_1_katas/session-kata-adalicious-21-3-25-IkuroMaira/front-client/src/components/menu/CardPlate.jsx
@@ -1,14 +1,14 @@
+import React from "react";
 import styles from './CardPlate.module.css';
-import image from '../../assets/icons/emoji_brocoli.png';
 
-export default function CardPlate() {
+export default function CardPlate({ plate }) {
     return (
         <>
             <div className={styles.platCard}>
-                <img className={styles.platImage} src={image} alt="Nom du plat"/>
+                <img className={styles.platImage} src={plate.image} alt="Nom du plat"/>
                 <div className={styles.platInfos}>
-                    <h1 className={styles.platTitle}>{}</h1>
-                    <p className={styles.platDescription}>{}</p>
+                    <h1 className={styles.platTitle}>{plate.name}</h1>
+                    <p className={styles.platDescription}>{plate.description}</p>
                     <button className={styles.btnOrder}>Commander</button>
                 </div>
             </div>
