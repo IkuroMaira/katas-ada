aab3b7087ce44f707419eb9dd055a56099c9e2f0
IkuroMaira <gwen.bussac@gmail.com>
2025-05-27 14:27:58 +0200

    Add exercise 001 of katas Ruby

---
+++ module_1_katas/session-katas-ruby-27-5-25/debutant/001_exo_age.rb
@@ -0,0 +1,19 @@
+# Exercice 1 :
+# Crée un programme qui demande à l'utilisateur son nom, son âge, et calcule en quelle année il/elle aura 100 ans.
+
+def giveAge(name, age)
+  puts "Pouvez-vous donner votre nom ?"
+  puts "Pouvez vous donner votre âge?"
+  puts "Bonjour #{name} !"
+  puts "Vous avez #{age} ans"
+
+  today = 2025
+  remainingYears = 100 - age
+  yearof100years = today + remainingYears
+
+  puts "Et vous aurez 100 ans en #{yearof100years}."
+end
+
+giveAge("Gwen", 31)
+giveAge("Ludo", 30)
+giveAge("Laurianne", 25)
